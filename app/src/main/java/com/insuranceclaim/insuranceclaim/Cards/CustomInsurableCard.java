eb6fb8d33b8c14a75556b247eaa106042351c05f
Kyrel Jerome <kyrel2007@gmail.com>
2018-07-21 14:12:38 -0400

    Fixed dead code due to lack of constructor in CustomInsurableCard TODO: Add more Insurable card types.

--- app/src/main/java/com/insuranceclaim/insuranceclaim/Cards/CustomInsurableCard.java
+++ app/src/main/java/com/insuranceclaim/insuranceclaim/Cards/CustomInsurableCard.java
@@ -1,10 +1,39 @@
 package com.insuranceclaim.insuranceclaim.Cards;
 
+import android.media.Image;
+import android.support.annotation.NonNull;
+
+import com.insuranceclaim.insuranceclaim.insurables.Insurable;
+
 /**
  * Created by kyrel_000 on 2018-06-21.
  * Originally created in InsuranceReport.
  */
 
-public abstract class CustomInsurableCard extends InsurableCard {
+public class CustomInsurableCard extends InsurableCard {
 
+
+    public CustomInsurableCard(Insurable insurable) {
+        super(insurable);
+    }
+
+    @Override
+    public Image getLogo() {
+        return null;
+    }
+
+    @Override
+    public String getHeader() {
+        return null;
+    }
+
+    @Override
+    public String getImportant() {
+        return null;
+    }
+
+    @Override
+    protected void createInsurable() {
+
+    }
 }
